fa4b219c0031008387fbdc07be71fecef4a36d62
Fabrice Bascoulergue <f.bascoulergue@gmail.com>
2014-04-29 20:36:42 +0200

    Allow CORS requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
+  before_filter :set_access_control_headers
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+    headers['Access-Control-Allow-Headers'] = '*'
+  end
 
   protected
   def authenticate
